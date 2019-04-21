e7b1eaa6c21c5404f960591fe5535aa56316c1c5
dmytsv <dmitrytsuprunov@Dmitrys-Mini.home>
2019-04-21 01:19:11 -0400

    X-Origin request fix

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const { mongoURI } = require("./config");
 const port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 MongoClient.connect(mongoURI, (err, database) => {
   if (err) return console.log(err);
 
