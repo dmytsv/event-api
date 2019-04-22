e1029b741fa60500ae55899efe8e1805edd794b5
dmytsv <dmitrytsuprunov@Dmitrys-Mini.home>
2019-04-21 23:00:23 -0400

    Delete x-origin fix v2

--- index.js
+++ index.js
@@ -10,15 +10,7 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET",
-    "PUT",
-    "PATCH",
-    "POST",
-    "DELETE",
-    "OPTIONS"
-  );
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
