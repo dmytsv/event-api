faf7e286f7ab18ef8bcaa33e52c326c682c5acc6
dmytsv <dmitrytsuprunov@Dmitrys-Mini.home>
2019-04-21 22:55:24 -0400

    Adding delete X-origin request

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
+    "Access-Control-Allow-Methods",
+    "GET",
+    "PUT",
+    "PATCH",
+    "POST",
+    "DELETE",
+    "OPTIONS"
+  );
+  res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
