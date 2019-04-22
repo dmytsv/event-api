c1eab4514b5251694a8c3ea315c262a41163121e
dmytsv <dmitrytsuprunov@Dmitrys-Mini.home>
2019-04-22 00:44:25 -0400

    Change parser to json

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const { mongoURI } = require("./config");
 const port = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
